1e17d270543f815d39fcb52cbb99d5d729ce6935
ControlledChaos0 <jade.spoon2003@gmail.com>
2023-09-29 13:17:39 -0400

    Fixable Location Basics

--- Assets/Scripts/MediumFixableLocation.cs
+++ Assets/Scripts/MediumFixableLocation.cs
@@ -6,10 +6,21 @@ public class MediumFixableLocation : MonoBehaviour
 {
     [SerializeField]
     private GameObject targetGO;
+    [SerializeField]
+    private float distanceToLock = 10.0f;
+    [SerializeField]
+    private float moveTime = 1.5f;
 
+    private Transform _targetTransform;
     private MeshRenderer _meshRenderer;
     private MeshFilter _meshFilter;
+    private MeshCollider _meshCollider;
     private LineRenderer _lineRenderer;
+    private Quaternion _baseTargetRot;
+    private Vector3 _baseTargetTrans;
+
+    private bool locked;
+    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +31,9 @@ public class MediumFixableLocation : MonoBehaviour
             _meshFilter.mesh = targetGO.GetComponent<MeshFilter>().mesh;
         }
         #endif
+        _targetTransform = targetGO.transform;
         _meshRenderer = targetGO.GetComponent<MeshRenderer>();
-        _lineRenderer = GetComponent<LineRenderer>();
+        //_lineRenderer = GetComponent<LineRenderer>();
         Vector3[] positions = {transform.position, targetGO.transform.position};
         _lineRenderer.SetPositions(positions);
     }
@@ -29,6 +41,19 @@ public class MediumFixableLocation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!locked && distanceToLock > Vector3.Distance(transform.position, _meshRenderer.gameObject.transform.position)) {
+            locked = true;
+            _baseTargetRot = _targetTransform.rotation;
+            _baseTargetTrans = _targetTransform.position;
+            startTime = 0.0f;
+        }
+        if (locked) {
+            _targetTransform.position = Vector3.Lerp(_baseTargetTrans, transform.position, startTime / moveTime);
+            _targetTransform.rotation = Quaternion.Lerp(_baseTargetRot, transform.rotation, startTime / moveTime);
+            startTime += Time.deltaTime;
+        }
+        if (startTime > moveTime) {
+            Destroy(gameObject);
+        }
     }
 }
